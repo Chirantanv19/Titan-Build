5c96bb3c11df68703210997f7a9d260a8d846cc2
Chirantan Tambe <chirantanv19@gmail.com>
2026-01-14 13:34:09 +0530

    text colour more generic colour

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -75,7 +75,7 @@ export default function Hero() {
           <span
             className="
       bg-gradient-to-r
-     from-orange-400 via-orange-500  to-orange-600 group
+     from-orange-200 via-orange-300  to-orange-400 group
       bg-clip-text text-transparent
       drop-shadow-[0_2px_2px_rgba(0,0,0,0.6)]
       italic
